6e92f5185e36c638119e024a3712860058783097
Cees Klap <ceesklap@gmail.com>
2020-06-23 17:44:41 +0200

    logo's toegevoegd

--- KnowledgeBoard/script.js
+++ KnowledgeBoard/script.js
@@ -1,12 +1,41 @@
 const radioButton = document.querySelectorAll(".selectButton");
 
 
+
 //ophalen van de data
 function searchsSource(choise){
    const findknowledge = KnowledgeSource.filter(filter =>{
        return filter.onderwerp.includes(choise)
    })
     console.log(findknowledge);
+    putOnScreen(choise, findknowledge);
+}
+
+function putOnScreen(logo, info){
+   
+    switch(logo){
+
+        case "CSS3" :
+            document.getElementById("logoImage").src ="images/CSS3.png";
+        break;
+
+        case "HTML5" :
+            document.getElementById("logoImage").src ="images/HTML5.png";
+        break;
+
+        case "Javescript" :
+            document.getElementById("logoImage").src ="images/Javascript.png";
+        break;
+
+        case "React" :
+            document.getElementById("logoImage").src ="images/React.png";
+        break;
+
+        case "Tools" :
+            document.getElementById("logoImage").src ="images/Tools.png";
+        break;
+
+    }
 }
 
 // event listener voor de radio buttons
