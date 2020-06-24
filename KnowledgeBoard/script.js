ead09bb95f69ce5577cc3b6e659ee7e22210a41f
Cees Klap <ceesklap@gmail.com>
2020-06-24 15:37:18 +0200

    JS is done

--- KnowledgeBoard/script.js
+++ KnowledgeBoard/script.js
@@ -1,48 +1,51 @@
 const radioButton = document.querySelectorAll(".selectButton");
-
-
+const getUl = document.getElementById("infoList");
 
 //ophalen van de data
-function searchsSource(choise){
-   const findknowledge = KnowledgeSource.filter(filter =>{
-       return filter.onderwerp.includes(choise)
-   })
-    console.log(findknowledge);
-    putOnScreen(choise, findknowledge);
+function searchsSource(choise) {
+  const findknowledge = KnowledgeSource.filter((filter) => {
+    return filter.onderwerp.includes(choise);
+  });
+
+  putOnScreen(choise, findknowledge);
+}
+// Het plaatsen van data in de DOM
+function putOnScreen(logo, info) {
+  switch (logo) {
+    case "CSS3":
+      document.getElementById("logoImage").src = "images/CSS3.png";
+      break;
+    case "HTML5":
+      document.getElementById("logoImage").src = "images/HTML5.png";
+      break;
+    case "Javescript":
+      document.getElementById("logoImage").src = "images/Javascript.png";
+      break;
+    case "React":
+      document.getElementById("logoImage").src = "images/React.png";
+      break;
+    case "Tools":
+      document.getElementById("logoImage").src = "images/Tools.png";
+      break;
+  }
+  
+  while (getUl.firstChild) {
+    getUl.firstChild.remove();
 }
 
-function putOnScreen(logo, info){
-   
-    switch(logo){
+  for(let i = 0; i < info.length; i++){
 
-        case "CSS3" :
-            document.getElementById("logoImage").src ="images/CSS3.png";
-        break;
-
-        case "HTML5" :
-            document.getElementById("logoImage").src ="images/HTML5.png";
-        break;
-
-        case "Javescript" :
-            document.getElementById("logoImage").src ="images/Javascript.png";
-        break;
-
-        case "React" :
-            document.getElementById("logoImage").src ="images/React.png";
-        break;
-
-        case "Tools" :
-            document.getElementById("logoImage").src ="images/Tools.png";
-        break;
-
-    }
+    let newLi = document.createElement("li"); 
+       
+    newLi.innerHTML= `<a target="_blank" href=${info[i].link}> ${info[i].name} </a>`; 
+    getUl.appendChild(newLi);
+  }
 }
 
 // event listener voor de radio buttons
 Array.from(radioButton).forEach((button) => {
-    button.addEventListener("change", () => {
-        const source = event.target.value; 
-        searchsSource(source);       
-    })
-    
+  button.addEventListener("change", () => {
+    const source = event.target.value;
+    searchsSource(source);
+  });
 });
